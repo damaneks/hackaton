3c8faf46d3664e2b0328cba9134303d4d0989d56
damaneks <56597480+damaneks@users.noreply.github.com>
2020-03-20 19:04:14 +0100

    Add files via upload

--- blob.js
+++ blob.js
@@ -14,13 +14,34 @@ function Blob(x, y, r, status, velx, vely, movement) {
 		this.pos.add(this.vel);
 	}
 
+	this.updateSim = function() {
+		this.vel.setMag(6);
+		if( movement == 1 ) {
+			this.vel = createVector(mouseX- width/2, mouseY-height/2);
+			this.vel.setMag(3);
+		}
+		this.inRangeSim();
+		this.pos.add(this.vel);
+	}
+
 	this.infection = function(other) {
 		var d = p5.Vector.dist(this.pos, other.pos);
 		if (d < this.r + other.r) {
 			if (other.status == "Infected")
 				this.status = "Infected"
-			this.vel.x = -this.vel.x;
-			this.vel.y = -this.vel.y;
+			
+			if ((other.vel.x == 0 && other.vel.y == 0) || (this.vel.x == 0 && this.vel.y == 0)) {
+				this.vel.x = -this.vel.x;
+				this.vel.y = -this.vel.y;
+			}else {
+				var pomx = this.vel.x;
+				var pomy = this.vel.y;
+				this.vel.x = other.vel.x;
+				this.vel.y = other.vel.y;
+				other.vel.x = pomx;
+				other.vel.y = pomy;
+			}
+			
 		}
 	}
 
@@ -29,6 +50,15 @@ function Blob(x, y, r, status, velx, vely, movement) {
 			this.vel.x = -this.vel.x;
 			this.vel.y = -this.vel.y;
 		}
+	}
+
+	this.inRangeSim = function() {
+		if(this.pos.x < this.r || this.pos.x > width-this.r ) {
+			this.vel.x = -this.vel.x;
+		} else if (this.pos.y < this.r || this.pos.y > height-this.r) {
+			this.vel.y = -this.vel.y;
+		}
+
 	}
 
 	this.show = function() {
--- sim.js
+++ sim.js
@@ -1,15 +1,26 @@
-var blob;
-
 var blobs = [];
-var numberOfHealthyBlobs = 400;
-var numberOfInfectedBlobs = 10;
+var numberOfHealthyBlobs = 100;
+var numberOfInfectedBlobs = 1;
 var percentOfRunning = 70;
+
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
-	blob = new Blob(width/2, height/2, 16, "Healthy", 0, 0, 1);
 	for( var i = 0; i < numberOfHealthyBlobs + numberOfInfectedBlobs; i++) {
-		var x = random(-width, width*2);
-		var y = random(-height, height*2);
+			while (true) {
+				var x = random(10,width-10);
+				var y = random(10,height-10);
+				var j = 0;
+				var posPom = createVector(x, y);
+				for(j; j < i; j++) {
+					var d = p5.Vector.dist(posPom, blobs[j].pos);
+					if( d <= blobs[j].r )
+						break;
+				}
+				if( j < i )
+					continue
+				else
+					break
+			}
 		var ifRunning = random(0, 100);
 		var velx = 0;
 		var vely = 0;
@@ -18,42 +29,26 @@ function setup() {
 			vely = random(-1,1)*random();
 		}
 		if( i < numberOfHealthyBlobs)
-			blobs[i] = new Blob(x, y, 16, "Healthy", velx, vely, 0);
+			blobs[i] = new Blob(x, y, 8, "Healthy", velx, vely, 0);
 		else
-			blobs[i] = new Blob(x, y, 16, "Infected", velx, vely, 0);
+			blobs[i] = new Blob(x, y, 8, "Infected", velx, vely, 0);
 	}
 }
 
 function draw() {
 	background(0);
-	translate(width/2-blob.pos.x, height/2-blob.pos.y);
 	
 	for(var i = 0; i < blobs.length; i++) {
 		blobs[i].show();
 	}
-
-	blob.show();
-	blob.update();
 	
 	for( var i = 0; i < blobs.length; i++) {
-		//blobs[i].show();
-		blob.infection(blobs[i]);
+		blobs[i].show();
 		for(var j = 0; j < blobs.length; j++){
 			if ( i != j)
 				blobs[i].infection(blobs[j]);
 		}
-		blobs[i].update();
-
+		blobs[i].updateSim();
 	}
 	
-	if(blob.status == "Infected") {
-		remove();	
-	}
-}
-
-function keyPressed () {
-	if(keyCode == ESCAPE) {
-		noLoop();
-		setup();
-	}
 }
